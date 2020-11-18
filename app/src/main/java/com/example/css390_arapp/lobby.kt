6315a61b2f04e6af6f5db7012865be30bfd35568
teabeans <twhlum@gmail.com>
2020-11-18 03:47:13 -0800

    Confirmed transfer of data from login screen to lobby activity.

--- app/src/main/java/com/example/css390_arapp/lobby.kt
+++ app/src/main/java/com/example/css390_arapp/lobby.kt
@@ -1,11 +1,21 @@
 package com.example.css390_arapp
 
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class lobby : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lobby)
+
+        // Get the Intent that started this activity and extract the string
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        // Capture the layout's TextView and set the string as its text
+        val textView = findViewById<TextView>(R.id.statusReport).apply {
+            text = message
+        }
     }
 }
