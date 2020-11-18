2cadc6f04ca48aa4188ef48d36f9ca2d26dea72a
Hao Duong <Howarddful123@gmail.com>
2020-11-18 04:10:40 -0800

    Location are currently save as altitude and longitude

--- app/src/main/java/com/example/css390_arapp/LocationCapture.kt
+++ app/src/main/java/com/example/css390_arapp/LocationCapture.kt
@@ -1,6 +1,7 @@
 package com.example.css390_arapp
 
 import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.widget.Button
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import java.io.IOException
 
 class LocationCapture : AppCompatActivity() {
     private lateinit var locationText : TextView
@@ -24,7 +26,17 @@ class LocationCapture : AppCompatActivity() {
 
         getLocationButton.setOnClickListener {
             if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-                updateLocation()
+                // permission granted, start getting current device location
+                fusedLocationClient.lastLocation.addOnSuccessListener { location: Location ->
+                    if(location != null){
+                        try{
+                            var altAndLong = location.altitude.toString() + " " + location.longitude
+                            locationText.text = altAndLong
+                        } catch (err : IOException){
+                            err.printStackTrace()
+                        }
+                    }
+                }
             }
             else{
                 // permission not granted
@@ -33,7 +45,4 @@ class LocationCapture : AppCompatActivity() {
         }
     }
 
-    private fun updateLocation() {
-        // TODO
-    }
 }
