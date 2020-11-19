726cedc885dc4c97b1c3af1cb0eb65cea45dc819
Hao Duong <Howarddful123@gmail.com>
2020-11-18 19:51:57 -0800

    Display the time of capture when the location is acquired

--- app/src/main/java/com/example/css390_arapp/LocationCapture.kt
+++ app/src/main/java/com/example/css390_arapp/LocationCapture.kt
@@ -3,7 +3,6 @@ package com.example.css390_arapp
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -11,10 +10,13 @@ import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class LocationCapture : AppCompatActivity() {
     private lateinit var locationText : TextView
     private lateinit var getLocationButton: Button
+    private lateinit var timeText : TextView
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +24,10 @@ class LocationCapture : AppCompatActivity() {
 
         locationText = findViewById(R.id.locationTextDisplay)
         getLocationButton = findViewById(R.id.getLocationButton)
+        timeText = findViewById(R.id.timeTextView)
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-
         getLocationButton.setOnClickListener {
+            //Check if permission is granted
             if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
                 // permission granted, start getting current device location
                 fusedLocationClient.lastLocation.addOnSuccessListener { location: Location ->
@@ -32,6 +35,7 @@ class LocationCapture : AppCompatActivity() {
                         try{
                             var altAndLong = "Altitude: " + location.altitude.toString() + ", Longitude:" + location.longitude
                             locationText.text = altAndLong
+                            timeText.text = "This location was update on: "+getDate(location.time)
                         } catch (err : IOException){
                             err.printStackTrace()
                         }
@@ -47,4 +51,9 @@ class LocationCapture : AppCompatActivity() {
         }
     }
 
+    private fun getDate(millis : Long) : String{
+        var dateFormat = "MM/dd/yyyy hh:mm:ss"
+        var formatter : SimpleDateFormat = SimpleDateFormat(dateFormat)
+        return formatter.format(Date(millis))
+    }
 }
