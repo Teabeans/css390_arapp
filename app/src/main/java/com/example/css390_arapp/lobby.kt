13ffd58d355202bbc6cc4909036505f8fbd6f492
teabeans <twhlum@gmail.com>
2020-11-18 20:08:58 -0800

    Database interface framework built in UI.

--- app/src/main/java/com/example/css390_arapp/lobby.kt
+++ app/src/main/java/com/example/css390_arapp/lobby.kt
@@ -1,8 +1,12 @@
 package com.example.css390_arapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class lobby : AppCompatActivity() {
@@ -17,5 +21,28 @@ class lobby : AppCompatActivity() {
         val textView = findViewById<TextView>(R.id.statusReport).apply {
             text = message
         }
+    }
+
+    // Send a key:value pair to the database
+    fun db_send(view: View) {
+        val sendKeyField = findViewById<EditText>(R.id.db_send_key)
+        val sendKey = sendKeyField.text.toString()
+
+        val sendValField = findViewById<EditText>(R.id.db_send_value)
+        val sendVal = sendValField.text.toString()
+
+        // Pop a toast to confirm keypress and Key:Value capture
+        val toaster = Toast.makeText( applicationContext, "DB_SEND - $sendKey : $sendVal", 2 )
+        toaster.show()
+    }
+
+    // Request information from the database
+    fun db_recv(view: View) {
+        val recvKeyField = findViewById<EditText>(R.id.db_recv_key)
+        val recvKey = recvKeyField.text.toString()
+
+        // Pop a toast to confirm keypress and Key capture
+        val toaster = Toast.makeText( applicationContext, "DB_RECV - $recvKey", 2 )
+        toaster.show()
     }
 }
