53c9587e218b277f5316dd9fce2e946feebc83c9
teabeans <twhlum@gmail.com>
2020-12-01 11:25:52 -0800

    Added ar_render activity

---
+++ app/src/main/java/com/example/css390_arapp/ar_render.kt
@@ -0,0 +1,11 @@
+package com.example.css390_arapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ar_render : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ar_render)
+    }
+}
