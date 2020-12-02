63f12cfe03a832509c38bdd2b484c8a26197e0f8
teabeans <twhlum@gmail.com>
2020-12-02 05:21:11 -0800

    Attempting to bring AR environment online; checkpoint reached.

--- app/src/main/java/com/example/css390_arapp/ar_render.kt
+++ app/src/main/java/com/example/css390_arapp/ar_render.kt
@@ -3,9 +3,31 @@ package com.example.css390_arapp
 // For location services
 
 // For intent catching
+
+// Ripped from AR-Core example
+
+import android.opengl.GLSurfaceView
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+
+/*
+import com.google.ar.core.examples.java.common.helpers.CameraPermissionHelper
+import com.google.ar.core.examples.java.common.helpers.DepthSettings
+import com.google.ar.core.examples.java.common.helpers.FullScreenHelper
+import com.google.ar.core.examples.java.common.helpers.InstantPlacementSettings
+import com.google.ar.core.examples.java.common.helpers.SnackbarHelper
+import com.google.ar.core.examples.java.common.helpers.TapHelper
+import com.google.ar.core.examples.java.common.helpers.TrackingStateHelper
+import com.google.ar.core.examples.java.common.samplerender.Framebuffer
+import com.google.ar.core.examples.java.common.samplerender.Mesh
+import com.google.ar.core.examples.java.common.samplerender.SampleRender
+import com.google.ar.core.examples.java.common.samplerender.Shader
+import com.google.ar.core.examples.java.common.samplerender.Texture
+import com.google.ar.core.examples.java.common.samplerender.VertexBuffer
+import com.google.ar.core.examples.java.common.samplerender.arcore.BackgroundRenderer
+import com.google.ar.core.examples.java.common.samplerender.arcore.PlaneRenderer
+*/
 
 class ar_render : AppCompatActivity() {
 
@@ -21,6 +43,8 @@ class ar_render : AppCompatActivity() {
         val tgtTxt = findViewById<TextView>(R.id.captured_coord)
         tgtTxt.text = message
 
+        // Rendering. The Renderers are created here, and initialized when the GL surface is created.
+        val surfaceView: GLSurfaceView? = findViewById<GLSurfaceView?>(R.id.surfaceview)
 
 
         // Acquire camera permissions
@@ -39,4 +63,7 @@ class ar_render : AppCompatActivity() {
         // AR magic?
 
     }
+
+
+
 }
